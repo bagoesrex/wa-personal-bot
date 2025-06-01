d6165171f7f17dbc8308347c7f62a41a6ee86f86
bagoesrex <bagoesrex@gmail.com>
2025-06-01 22:34:29 +0700

    chore: initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
+
+const client = new Client({
+  authStrategy: new LocalAuth({
+    dataPath: "./session",
+  }),
+});
+
+client.once("ready", () => {
+  console.log("✅ Client is ready!");
+});
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+  console.log("🔐 QR code received, scan once!");
+});
+
+client.initialize();
