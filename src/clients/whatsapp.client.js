b84d729f0db78f04d36a6ad10ec8a6d0939b21b6
bagoesrex <bagoesrex@gmail.com>
2026-01-17 16:42:02 +0700

    fix: pin whatsapp web version using web version cache

--- src/clients/whatsapp.client.js
+++ src/clients/whatsapp.client.js
@@ -9,5 +9,10 @@ export function createWhatsappClient(whatsappConfig) {
     }),
     deviceName: whatsappConfig.deviceName,
     puppeteer: whatsappConfig.puppeteer,
+    // temp
+    webVersionCache: {
+      type: "remote",
+      remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/refs/heads/main/html/2.3000.1031490220-alpha.html`,
+    },
   });
 }
